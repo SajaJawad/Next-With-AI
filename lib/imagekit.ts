26dafcd61ee0b713c590d70ee84130bcf18dc352
SajaJawad <saiamaher2002@gmail.com>
2026-04-28 14:15:40 +0300

    feat: add ImageKit configuration and buffer upload utility to lib/imagekit.ts

--- lib/imagekit.ts
+++ lib/imagekit.ts
@@ -1,13 +1,11 @@
-import { ImageKit, toFile } from "@imagekit/nodejs";
+import ImageKit, { toFile } from "@imagekit/nodejs";
 
-let _client: InstanceType<typeof ImageKit> | null = null;
+let _client: ImageKit | null = null;
 
 function getClient() {
   if (!_client) {
     _client = new ImageKit({
-      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
-      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
     });
   }
   return _client;
