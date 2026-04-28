1756511551a21449c401975a3443b3db27b7db60
SajaJawad <saiamaher2002@gmail.com>
2026-04-28 13:34:34 +0300

    implement StudioWorkbenchContext for managing image generation state and workflow

--- context/StudioWorkbenchContext.tsx
+++ context/StudioWorkbenchContext.tsx
@@ -189,7 +189,6 @@ function useStudioWorkbenchValue({
           token: authParams.token,
           expire: authParams.expire,
           publicKey: authParams.publicKey,
-          urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
         });
 
         if (!uploadResult.url) {
